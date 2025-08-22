000a2befa35a349ca5f4ff1274a9e0f50fcdbf68
Viktor Mirjanic <vvm22@cam.ac.uk>
2025-08-22 18:32:50 +0100

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# abc
+# WAM Divergence 
+
+This is a Lean 4 supplement for the [paper]() and formalises the proof in Section 2.# 
+The main statement is in `DivergencePow2.lean`.
--- WAM.lean
+++
@@ -1,5 +0,0 @@
--- This module serves as the root of the `Abc` library.
--- Import modules here that should be built as part of the library.
-import WAM.Basic
-import WAM.Defs
-import WAM.DivergencePow2
